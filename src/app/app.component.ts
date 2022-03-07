0e4bd26f5eb0ee1b446dcf5403bed50c6f3298d1
Miguel Palacios <Perueduca54@minedu.gob.pe>
2022-03-07 14:19:22 -0500

    subida_v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { appState } from './app.reducer';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromMensaje from './mensaje.action'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'appRedux';
+  dato$: Observable<any>;
+  
+  constructor(private store : Store<appState>) {
+    this.dato$ = this.store.select<any>('mensaje');         
+  }
+
+  spanishMensaje() {
+    this.store.dispatch(new fromMensaje.SpanishMensaje('Los colores'));
+  }
+  englishMensaje() {
+    this.store.dispatch(new fromMensaje.EnglishMensaje('yellow'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store'
+import { miReducer } from './app.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ mensaje: miReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
